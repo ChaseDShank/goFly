c239d02384ad9aa5660af8ed3c39de92243d9f4a
Chase Shank <216cds@gmail.com>
2018-02-02 12:39:03 -0700

    map working

---
+++ gF/src/store/index.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import vue from 'vue'
+import vuex from 'vuex'
+import router from 'router'
+import debug from 'util'
+import Vue from 'vue'
+
+
+
+vue.use(vuex)
+
+
+
+
+
+var store = new vuex.Store({
+
+    state: {
+        map: {},
+    },
+    mutations: {
+        
+    },
+
+    actions: {
+      
+    }
+})
+
+
+export default store
